49b9f2cb2a16bdfdff47de7227979904ef147032
David Velasquez <david.v@wagento.com>
2015-02-15 18:07:28 -0400

    practica layout chapter 4

---
+++ app/code/local/David/Dev18cap4/controllers/indexController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v1t0
+ * Date: 2/15/15
+ * Time: 5:13 PM
+ */
+
+class David_Dev18cap4_IndexController extends Mage_Core_Controller_Front_Action{
+    public function indexAction(){
+        $this->loadLayout();
+        $this->renderLayout();
+        echo "index!!!";
+    }
+
+    public function helloAction(){
+        $this->loadLayout();
+        $this->renderLayout();
+        echo "hello...!!!";
+    }
+}
