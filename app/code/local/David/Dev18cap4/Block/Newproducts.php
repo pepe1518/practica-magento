865f0a60a7a1780b83cf42e5bdaf969fb83ec24f
David Velasquez <david.v@wagento.com>
2015-02-15 19:23:33 -0400

    added translation file and template file practice for chapter 4

---
+++ app/code/local/David/Dev18cap4/Block/Newproducts.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v1t0
+ * Date: 2/15/15
+ * Time: 7:06 PM
+ */
+class David_Dev18cap4_Block_Newproducts extends Mage_Core_Block_Template{
+
+}
--- app/code/local/David/Dev18cap4/controllers/indexController.php
+++ app/code/local/David/Dev18cap4/controllers/indexController.php
@@ -10,7 +10,7 @@ class David_Dev18cap4_IndexController extends Mage_Core_Controller_Front_Action{
     public function indexAction(){
         $this->loadLayout();
         $this->renderLayout();
-        echo "index!!!";
+        echo $this->__('David');
     }
 
     public function helloAction(){
