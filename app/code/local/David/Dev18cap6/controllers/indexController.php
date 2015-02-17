560970fd3a4c75a41c8df204f3746f6b59c70362
David Velasquez <david.v@wagento.com>
2015-02-17 13:40:19 -0400

    registering from resource model

---
+++ app/code/local/David/Dev18cap6/controllers/indexController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v1t0
+ * Date: 2/17/15
+ * Time: 1:21 PM
+ */
+
+class David_Dev18cap6_IndexController extends Mage_Core_Controller_Front_Action{
+    public function indexAction(){
+        echo "hola cap6";
+    }
+}
