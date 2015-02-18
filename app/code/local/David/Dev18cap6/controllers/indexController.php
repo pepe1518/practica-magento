f31b66ccbd8d2c7f4df9b7cc378a24bb492e4036
David Velasquez <david.v@wagento.com>
2015-02-18 01:08:15 -0400

    added supbcription actio on index controller

--- app/code/local/David/Dev18cap6/controllers/indexController.php
+++ app/code/local/David/Dev18cap6/controllers/indexController.php
@@ -10,4 +10,15 @@ class David_Dev18cap6_IndexController extends Mage_Core_Controller_Front_Action{
     public function indexAction(){
         echo "hola cap6";
     }
+    public function subscriptionAction(){
+        echo "subscribtion";
+        $subscription = Mage::getModel('dev18cap6/subscription');
+        echo "<br>".get_class($subscription);
+        $subscription->setFirstname('David');
+        $subscription->setLastname('Velasquez');
+        $subscription->setEmail('jdavidvr@hotmail.com');
+        $subscription->setMessage('this is a short message to test model created');
+        $subscription->save();
+        echo "<br>success";
+    }
 }
