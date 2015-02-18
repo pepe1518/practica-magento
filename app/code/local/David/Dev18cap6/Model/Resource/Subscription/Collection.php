6c389ed721eabf9fbca63f34f91bfc7786bc60ff
David Velasquez <david.v@wagento.com>
2015-02-18 00:53:00 -0400

    test method save load from DB created

---
+++ app/code/local/David/Dev18cap6/Model/Resource/Subscription/Collection.php
@@ -0,0 +1,10 @@
+<?php
+
+class David_Dev18cap6_Model_Resources_Subscription_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
+{
+
+    protected function _construct()
+    {
+        $this->_init('dev18cap6/subscription');
+    }
+}
--- app/code/local/David/Dev18cap6/Model/Subscription.php
+++ app/code/local/David/Dev18cap6/Model/Subscription.php
@@ -1,10 +1,9 @@
 <?php
-class David_Dev18cap4_Model_Subscription extends Mage_Core_Model_Abstract
-{
+class David_Dev18cap6_Model_Subscription extends Mage_Core_Model_Abstract{
 
     protected function _construct()
     {
-        $this->_init('dev18cap4/subscription');
+        $this->_init('dev18cap6/subscription');
     }
 
 }
