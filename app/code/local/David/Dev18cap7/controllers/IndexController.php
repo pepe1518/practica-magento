c05654eb9c25652879efba3ec9625edf6be4eb24
David Velasquez <david.v@wagento.com>
2015-02-23 00:12:30 -0400

    grid read from DB partial work still needs works

--- app/code/local/David/Dev18cap7/controllers/IndexController.php
+++ app/code/local/David/Dev18cap7/controllers/IndexController.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: v1t0
- * Date: 2/22/15
- * Time: 12:48 AM
- */
 class David_Dev18cap7_IndexController extends Mage_Core_Controller_Front_Action{
 
     public function indexAction(){
