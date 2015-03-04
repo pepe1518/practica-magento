d5a8ed2d0b6ecd0a1ed8be30e141abb72eacaf9f
jose@wagento.com <jose@wagento.com>
2015-03-04 14:55:36 -0400

    [new modulo]add new files

---
+++ app/code/local/Jose/Holapepe/Helper/Data.php
@@ -0,0 +1,4 @@
+<?php
+class Jose_Holapepe_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/Jose/Holapepe/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+class Jose_Holapepe_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction() {
+
+    }
+
+    public function helloAction() {
+        echo "estes es un hola mundo desde el action de un controlador";
+    }
+}
