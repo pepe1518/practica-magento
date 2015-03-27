c150fff235eeaed5d84723b4084e8db4ecb76551
jose@wagento.com <jose@wagento.com>
2015-03-27 16:32:16 -0400

    [feedback]creating architecture of module

---
+++ app/code/local/Jose/Holamundo/Helper/data.php
@@ -0,0 +1,5 @@
+<?php
+class Jose_Holamundo_Helper_data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ app/code/local/Jose/Holamundo/controllers/indexController.php
@@ -0,0 +1,11 @@
+<?php
+class Jose_Holamundo_indexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction() {
+
+    }
+
+    public function helloAction() {
+        echo 'Action Hello in de helloworld index controller';
+    }
+}
